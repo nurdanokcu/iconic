3ef6e88aec6a0b46ae87c157d669fc470d8e58d5
nurdanokcu <nurokcudan@gmail.com>
2024-12-17 20:51:41 +0100

    og images are added

--- app.vue
+++ app.vue
@@ -3,6 +3,9 @@ const title = 'Iconic Modeling Agency';
 const description = 'Iconic is a new modeling agency in Germany, but we are already making waves. We represent some of the most promising new models in the country, and we are committed to helping them achieve their modeling goals.';
 
 useHead({
+  htmlAttrs: {
+    lang: 'en',
+  },
   link: [
     {
       rel: 'icon',
@@ -23,12 +26,6 @@ useSeoMeta({
   twitterDescription: description,
   twitterImage: '/twitter-image.png',
   twitterCard: 'summary',
-});
-
-useHead({
-  htmlAttrs: {
-    lang: 'en',
-  },
 });
 </script>
 
